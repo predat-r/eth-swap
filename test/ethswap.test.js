74153fd7e176015eb409f9deec320ba6d654ed48
predat-r <harisnaeem612@outlook.com>
2025-01-25 17:02:45 +0500

    Initialization

---
+++ test/ethswap.test.js
@@ -0,0 +1,5 @@
+const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token");
+require('chai').use(require('chai-as-promised')).should()
+
+contract("EthSwap", (accounts) => {});
