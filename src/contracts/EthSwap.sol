4a07fcaba4d99cbdea2ffdea935307249af2f5fc
predat-r <harisnaeem612@outlook.com>
2025-01-26 18:22:12 +0500

    buy and sell functions in the smart contract

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -6,8 +6,33 @@ contract EthSwap {
     Token public token;
     uint public rate = 100;
 
+    event TokensPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+    event TokensSold(address account, address token, uint amount, uint rate);
     constructor(Token _token) public {
-      const amountOfTokens = msg.value * rate; 
-      token = _token;
+        token = _token;
+    }
+    function buyTokens() public payable {
+        uint amountOfTokens = msg.value * rate;
+        token.transfer(msg.sender, amountOfTokens);
+
+        require(token.balanceOf(address(this)) >= amountOfTokens);
+
+        emit TokensPurchased(msg.sender, address(token), amountOfTokens, rate);
+    }
+
+    function sellTokens(uint _amount) public {
+        require(token.balanceOf(msg.sender) >= _amount);
+        uint amountOfTokens = _amount / rate;
+
+        require(address(this).balance >= amountOfTokens);
+        token.transferFrom(msg.sender, address(this), _amount);
+        msg.sender.transfer(amountOfTokens);
+
+        emit TokensPurchased(msg.sender, address(token), _amount, rate);
     }
 }
--- test/ethswap.test.js
+++ test/ethswap.test.js
@@ -1,3 +1,5 @@
+const { assert } = require("chai");
+
 const EthSwap = artifacts.require("EthSwap");
 const Token = artifacts.require("Token");
 require("chai")
@@ -7,7 +9,7 @@ require("chai")
 function tokens(n) {
   return web3.utils.toWei(n, "ether");
 }
-contract("EthSwap", (accounts) => {
+contract("EthSwap", ([deployer, investor]) => {
   let token, ethswap;
   before(async () => {
     token = await Token.new();
@@ -36,4 +38,59 @@ contract("EthSwap", (accounts) => {
       );
     });
   });
+
+  describe("buy tokens", async () => {
+    let result;
+    before(async () => {
+      result = await ethswap.buyTokens({
+        from: investor,
+        value: web3.utils.toWei("1", "ether"),
+      });
+    });
+    it("allows user to instantly purchase tokens from ethSwap for a fixed price", async () => {
+      let investorBalance = await token.balanceOf(investor);
+      assert.equal(investorBalance.toString(), tokens("100"));
+
+      let ethSwapBalance;
+      ethSwapBalance = await token.balanceOf(ethswap.address);
+      assert.equal(ethSwapBalance.toString(), tokens("999900"));
+
+      ethSwapBalance = await web3.eth.getBalance(ethswap.address);
+      assert.equal(ethSwapBalance, web3.utils.toWei("1", "ether"));
+
+      const event = result.logs[0].args;
+      assert.equal(event.account, investor);
+      assert.equal(event.token, token.address);
+      assert.equal(event.amount.toString(), tokens("100").toString());
+      assert.equal(event.rate.toString(), "100");
+    });
+  });
+
+  describe("sell tokens", async () => {
+    let result;
+    before(async () => {
+      await token.approve(ethswap.address, tokens("100"), { from: investor });
+      result = await ethswap.sellTokens(tokens("100"), {
+        from: investor,
+      });
+    });
+    it("allows user to instantly sell tokens to ethSwap for a fixed price", async () => {
+      let investorBalance = await token.balanceOf(investor);
+      assert.equal(investorBalance.toString(), tokens("0"));
+
+      let ethSwapBalance;
+      ethSwapBalance = await token.balanceOf(ethswap.address);
+      assert.equal(ethSwapBalance.toString(), tokens("1000000"));
+      ethSwapBalance = await web3.eth.getBalance(ethswap.address);
+      assert.equal(ethSwapBalance, web3.utils.toWei("0", "ether"));
+
+      const event = result.logs[0].args;
+      assert.equal(event.account, investor);
+      assert.equal(event.token, token.address);
+      assert.equal(event.amount.toString(), tokens("100").toString());
+      assert.equal(event.rate.toString(), "100");
+
+      await ethswap.sellTokens(tokens('500'),{from:investor}).should.be.rejected;
+    });
+  });  
 });
